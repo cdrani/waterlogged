bb5b733287140a8d4f691b017c596cba2ad6e2fd
cdrani <charlesdrani@gmail.com>
2024-06-05 13:31:57 -0600

    ui: structure temp ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,16 @@
 <script lang="ts">
+    import Button from "./components/Button.svelte"
 </script>
 
-<h1>Water Log: Drowning Down the River Styx</h1>
+<main class="flex flex-col bg-white w-full h-full">
+    <div class="flex flex-col p-4">
+        <h1 class="text-gray-900 font-bold text-2xl">09:00 AM</h1>
+        <h3 class="text-gray-500 font-semibold text-lg">300ml water</h3>
+    </div>
+    <div class="flex h-full bg-[#68B6FF]">
+        <div class="flex gap-4 w-1/2 p-4">
+            <Button text="+" handler={() => {}} />
+            <Button text="-" handler={() => {}} />
+        </div>
+    </div>
+</main>
---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let text: string
+    export let handler: (e: MouseEvent) => void
+</script>
+
+<button 
+    on:click={handler}
+    class="text-white inline-flex justify-center items-center text-2xl w-10 h-10 bg-cyan-400 rounded-full">
+    {text}
+</button>
