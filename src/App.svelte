6789b99dd130584818ee83c87414c3c93bd03881
cdrani <charlesdrani@gmail.com>
2024-06-06 14:56:43 -0600

    refactor: use DateStore to sync ui with store

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,37 @@
 <script lang="ts">
-    import { writable } from "svelte/store";
+    import { onMount } from "svelte";
     import Button from "./components/Button.svelte"
     import CurrentTime from "./components/CurrentTime.svelte";
 
-    const waterMeasurement = writable<'ml' | 'cup'>('ml')
-    const waterIntake = writable<number>(300)
+    import DataStore from './stores'
 
-    function incrementAmount() {
-        const amount = $waterMeasurement == 'ml' ? 100 : 1
-        waterIntake.update(previousAmount => previousAmount + amount)
-    }
-    
-    function decrementAmount() {
-        const amount = $waterMeasurement == 'ml' ? 100 : 1
-        waterIntake.update(previousAmount => Math.max(0, previousAmount - amount))
-    }
+    let PORT = chrome.runtime.connect({ name: 'popup' })
+    const store = new DataStore(PORT)
+
+    onMount(async () => {
+        store.populate()
+        PORT.onMessage.addListener(async () => {
+            console.log('port listening on popup')
+        })
+
+        return() => {
+            PORT = null
+        }
+    })
+
+    $: total = store.total
+    $: measurement = store.measurement
 </script>
 
 <main class="flex flex-col bg-white w-full h-full">
     <div class="flex flex-col p-4">
         <CurrentTime />
-        <h3 class="text-gray-500 font-semibold text-lg">{$waterIntake}{$waterMeasurement} drank</h3>
+        <h3 class="text-gray-500 font-semibold text-lg">{$total}{$measurement} drank</h3>
     </div>
     <div class="flex h-full bg-[#68B6FF]">
         <div class="flex gap-4 w-1/2 p-4">
-            <Button text="-" handler={decrementAmount} />
-            <Button text="+" handler={incrementAmount} />
+            <Button text="-" handler={() => store.decrementAmount() } />
+            <Button text="+" handler={() => store.incrementAmount() } />
         </div>
     </div>
 </main>
