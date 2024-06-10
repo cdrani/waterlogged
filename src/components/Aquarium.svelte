d02160bfa12920ecc9215dd871ac2f84cbd531be
cdrani <charlesdrani@gmail.com>
2024-06-10 12:04:23 -0600

    styles: align total & goal values visually

--- src/components/Aquarium.svelte
+++ src/components/Aquarium.svelte
@@ -12,12 +12,14 @@
 <section class="relative -z-100 bg-cyan-500 flex flex-col p-4 w-full h-[120px]">
     <div class="flex relative z-10 mb-4 w-full justify-between">
         <CurrentTime />
-        <div>
-            <p class="font-semibold text-[14px] text-black">
-                Total: {$total}{$today.measurement}
+        <div class="flex flex-col min-w-[100px] max-w-3/5 items-end">
+            <p class="flex justify-between font-semibold text-[14px] text-black w-full">
+                <span class="inline-flex text-left">Total:</span>
+                <span class="inline-flex justify-end">{$total}{$today.measurement}</span>
             </p>
-            <p class="font-semibold text-[14px] text-black">
-                Goal: {$today.goal}{$today.measurement}
+            <p class="flex justify-between font-semibold text-[14px] text-black w-full">
+                <span class="inline-flex text-left">Goal:</span>
+                <span class="inline-flex justify-end">{$today.goal}{$today.measurement}</span>
             </p>
         </div>
     </div>
