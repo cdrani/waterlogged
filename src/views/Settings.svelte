58f41a01fe7bc6fa942e6260c14d949821112f26
cdrani <charlesdrani@gmail.com>
2024-06-11 12:59:03 -0600

    styles: set height + padding on inputs

--- src/views/Settings.svelte
+++ src/views/Settings.svelte
@@ -23,27 +23,37 @@
 
         store.updateSetting({ key, value })
     }
+
+    const inputClass = "px-0.5 h-7 text-[14px] rounded-[4px]"
+
 </script>
 
-<section class="flex flex-col gap-4 bg-cyan-400 h-full w-full p-4">
-    <form class="flex flex-col w-full gap-5">
+<section class="flex flex-col gap-4 bg-cyan-400 h-full w-full p-4 rounded-md">
+    <form class="flex flex-col w-full gap-4">
         <label class="flex justify-between">
             <span class="text-[14px]">Enabled</span>
-            <input type="checkbox" name="enabled" checked={$settings.enabled} on:change={handleInput} />
+            <input 
+                type="checkbox"
+                name="enabled"
+                checked={$settings.enabled}
+                on:change={handleInput}
+            />
         </label>
 
         <div class="flex justify-between w-full gap-x-6">
             <label class="flex flex-col w-1/2 gap-y-1">
                 <span class="text-[14px]">Start Time</span>
                 <input
-                    type="time" name="start_time" value={$settings.start_time} class="text-sm text-end"
+                    type="time" name="start_time" value={$settings.start_time}
+                    class="text-end {inputClass}"
                     on:change={handleInput}
                 />
             </label>
 
             <label class="flex flex-col justify-end w-1/2 gap-y-1">
                 <span class="text-[14px] text-end">End Time</span>
-                <input type="time" name="end_time" value={$settings.end_time} class="text-end"
+                <input type="time" name="end_time" value={$settings.end_time}
+                    class="text-end {inputClass}"
                     on:change={handleInput}
                 />
             </label>
@@ -51,7 +61,7 @@
 
         <label class="flex justify-between gap-6">
             <span class="text-[14px] w-1/2">Measurement</span>
-            <select name="measurement" value={$settings.measurement} class="w-1/2 text-end"
+            <select name="measurement" value={$settings.measurement} class="w-1/2 text-end {inputClass}"
                 on:change={handleInput}
             >
                 <option value="ml">ml</option>
@@ -62,14 +72,14 @@
         <div class="flex justify-between w-full gap-x-6">
             <label class="flex flex-col w-1/2 gap-y-1">
                 <span class="text-[14px]">Goal (ml)</span>
-                <input name="goal" type="number" value={$settings.goal} class="w-full text-end"
+                <input name="goal" type="number" value={$settings.goal} class="w-full text-end {inputClass}"
                     on:change={handleInput}
                 />
             </label>
 
             <label class="flex flex-col w-1/2 gap-y-1">
                 <span class="text-[14px] text-end">Intake (in ml)</span>
-                <input name="intake" type="number" value={$settings.intake} class="w-full text-end"
+                <input name="intake" type="number" value={$settings.intake} class="w-full text-end {inputClass}"
                     on:change={handleInput}
                 />
             </label>
@@ -78,14 +88,14 @@
 
         <label class="flex justify-between w-full gap-x-6">
             <span class="text-[14px] w-1/2">Timer (mins)</span>
-            <input name="interval" type="number" value={$settings.interval} class="w-1/2 text-end"
+            <input name="interval" type="number" value={$settings.interval} class="w-1/2 text-end {inputClass}"
                     on:change={handleInput}
             />
         </label>
 
         <label class="flex justify-between gap-y-1 gap-x-6">
             <span class="text-[14px] w-1/2">Alert Type</span>
-            <select name="alert_type" value={$settings.alert_type} class="w-1/2 text-end"
+            <select name="alert_type" value={$settings.alert_type} class="w-1/2 text-end {inputClass}"
                     on:change={handleInput}
             >
                 <option value="notification">Notification</option>
