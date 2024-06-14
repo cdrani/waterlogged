0daab2e597ae0bf85f979c01ef01e962b9fca5f2
cdrani <charlesdrani@gmail.com>
2024-06-14 17:27:07 -0600

    feat: register Modal and modal store

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,33 @@
     import { writable } from 'svelte/store'
 
     import Nav from './components/Nav.svelte'
+    import Modal from './components/Modal.svelte'
     import DefaultView from "./views/Default.svelte"
     import SettingsView from './views/Settings.svelte'
 
+    import { initModal, getModal } from './stores/modal'
     import TodayStore from './stores/today'
     import SettingsStore from './stores/settings'
 
     let PORT = chrome.runtime.connect({ name: 'popup' })
     
+    initModal()
+
+    const modal = getModal()
     const today = new TodayStore(PORT)
     const settings = new SettingsStore(PORT, today)
 
-    type View = 'default' | 'settings'
-    let view = writable<View>('default')
+    type PageView = 'default' | 'settings'
+    let pageView = writable<PageView>('default')
 
     function setView(event: CustomEvent) {
         const { newView } = event.detail
-        view.set(newView) 
+        pageView.set(newView) 
         newView == 'default' ? today.populate() : settings.populate()
     }
 
     onMount(() => {
-        $view == 'default' ? today.populate() : settings.populate()
+        $pageView == 'default' ? today.populate() : settings.populate()
         return() => {
             PORT.onDisconnect.addListener(() => (PORT = null))
         }
@@ -32,11 +37,16 @@
 </script>
 
 <main class="relative -z-100 bg-cyan-500 flex flex-col bg-transparent p-4 pt-0 w-[280px] h-[360px]">
-    <Nav view={$view} on:view={setView} />
+    <Nav view={$pageView} on:view={setView} />
+    <Modal />
 
-    {#if $view == 'default'}
-        <DefaultView store={today} />
-    {:else}
-        <SettingsView store={settings} />
-    {/if}
+    <div 
+        class="{$modal.visible ? 'shadow-black blur-md opacity-75' : ''}"
+    >
+        {#if $pageView == 'default'}
+            <DefaultView store={today} />
+        {:else}
+            <SettingsView store={settings} />
+        {/if}
+    </div>
 </main>
