11846d06949b601c6d72552baf3583d1c4c73365
cdrani <charlesdrani@gmail.com>
2024-06-15 22:00:33 -0600

    feat: create Celebrate component

---
+++ src/components/Celebrate.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Confetti } from 'svelte-confetti'
+
+    export let party: boolean = false 
+</script>
+
+{#if party}
+    <div class="fixed pointer-events-none h-screen w-screen flex justify-center left-0 top-[-50px] z-100000">
+        <Confetti x={[-5, 5]} y={[0, 0.1]} infinite duration=5000 amount=2000 fallDistance="300vh" />
+    </div>
+{/if}
