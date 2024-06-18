56dc367cc33bbbc258e43d52458c9c8aa6a5b06b
cdrani <charlesdrani@gmail.com>
2024-06-18 04:55:41 -0600

    feat: reset notification timers on state change

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import { setState, getState } from './utils/state'
+import Notification from './utils/notifications'
 
 let POPUP_PORT = null
 
@@ -16,6 +17,60 @@ function mergeObjects(base, other) {
     }
     return base
 }
+
+const Notifier = new Notification()
+
+chrome.runtime.onInstalled.addListener(async () => {
+    await setState({ 
+        key: 'settings', 
+        values: {
+            goal: 1800,
+            intake: 100,
+            interval: 60, // minutes
+            enabled: true,
+            measurement: 'ml',
+            end_time: '18:00',
+            start_time: '08:00',
+            alert_type: 'notify'
+        }
+    })
+
+    await setState({ 
+        key: 'today', 
+        values: {
+            intake: 100,
+            logs: [],
+            goal: 1800,
+            measurement: 'ml'
+        }
+    })
+
+    Notifier.updateConfig({
+        interval: 60, // minutes
+        enabled: true,
+        end_time: '18:00',
+        start_time: '08:00',
+        alert_type: 'notify'
+    })
+
+    Notifier.welcome()
+    await Notifier.startTimer()
+})
+
+chrome.storage.onChanged.addListener(async (changes) => {
+    const isSettings = Object.keys(changes)?.at(0) == 'settings'
+    if (!isSettings) return
+
+    const { oldValue, newValue } = changes.settings
+    const { enabled, end_time, start_time, interval, alert_type } = newValue
+    Notifier.updateConfig({ enabled, end_time, start_time, interval, alert_type })
+
+    if ((oldValue.enabled !== newValue.enabled)) {
+        newValue.enabled && newValue.alert_type !== 'none' ? Notifier.startTimer() : Notifier.clearTimer()
+    } else if (oldValue.alert_type !== newValue.alert_type) {
+        newValue.enabled && newValue.alert_type !== 'none' ? Notifier.startTimer() : Notifier.clearTimer()
+    }
+})
 
 chrome.runtime.onConnect.addListener(async (port) => {
     if (port.name !== 'popup') return
