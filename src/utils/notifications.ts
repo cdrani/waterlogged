96142ec4524471b14c1d63631a69eba5d01539d0
cdrani <charlesdrani@gmail.com>
2024-06-25 22:54:15 -0600

    feat: show progress & support in notification

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,79 +1,139 @@
 import { getState } from './state'
+import { getDateKey, getDateMS } from './date'
+import { ENCOURAGEMENTS } from './encouragements'
+import type { DAILY_RESPONSE, SETTINGS, TODAY } from './types.d'
 import { ensureOffscreenDocument, sendOffscreenMessage } from './offscreen'
 
-type NotificationType = 'install' | 'notify'
-const OFFSCREEN_FILE_PATH = 'src/offscreen.html'
+const FULL_DAY_MS = 60 * 60 * 24 * 1000
+
+type Progress = { goal: number, left: number, percentage: number }
 
 export default class Notification {
+    _timer: Timer | null
+    _today: TODAY | null
+    _settings: SETTINGS | null
+    _initialTimeout: Timer | null
+
     constructor() {
         this._timer = null
-    }
-
-    async startTimer() {
-        let iteration = 0
-
-        this._timer = setInterval(async () => {
-            const { settings } = await getState(['settings'])
-            if (!settings) return
-            const { enabled, sound, start_time, end_time, interval, alert_type } = settings
-
-            if (!enabled || alert_type == 'none') return this.clearTimer()
-            if (!this.checkWithinBoundary({ start_time, end_time })) return
-            iteration++
-
-            if (iteration >= Number(interval)) {
-                ;['notify', 'both'].includes(alert_type) && this.notify()
-                ;['alarm', 'both'].includes(alert_type) && this.playSound(sound)
-                iteration = 0
-            }
-        }, 60_000)
-    }
-
-    checkWithinBoundary({ start_time, end_time }) {
-        const now = (new Date()).getTime()
-        const startMS = this.getDateMS(start_time)
-        const endMS = this.getDateMS(end_time)
-        const withinBoundary = (startMS <= now && now < endMS)
-        return withinBoundary
-    }
-
-    getDateMS(timeStr: string) {
-        const [hour, min] = timeStr.split(':')
-        const date = new Date()
-        date.setHours(Number(hour))
-        date.setMinutes(Number(min))
-        return date.getTime()
-    }
-
-    clearTimer() {
-        if (this._timer) clearInterval(this._timer)
-        this._timer = null
+        this._settings = null
+        this._today = null
+        this._initialTimeout = null
     }
 
     welcome() {
-        this.create('install')
+        this.#createWelcome()
     }
 
-    notify() {
-        this.create('notify')
+    #getTimeBoundaries() {
+        const { start_time, end_time } = this._settings
+        const nowMS = getDateMS()
+        const startMS = getDateMS(start_time)
+        let endMS = getDateMS(end_time)
+
+        if (endMS <= startMS) { endMS += FULL_DAY_MS }
+
+        return { nowMS, startMS, endMS }
     }
 
-    async playSound(sound: string) {
-        await ensureOffscreenDocument(OFFSCREEN_FILE_PATH)
+    async #getSettings() {
+        const { settings } = await getState('settings')
+        if (settings) (this._settings = settings)
+        return settings
+    }
+
+    #clearTimers() {
+        if (this._timer) clearInterval(this._timer)
+        if (this._initialTimeout) clearTimeout(this._initialTimeout)
+
+        this._timer = null
+        this._initialTimeout = null
+    }
+
+    async startTimer() {
+        const settings = await this.#getSettings()
+        if (!settings) return
+
+        if (!this._settings.enabled || this._settings.alert_type == 'none') return this.#clearTimers()
+
+        const { nowMS, startMS, endMS } = this.#getTimeBoundaries()
+        this.#clearTimers()
+
+        // Calculate initial delay until the start time
+        let delay = startMS - nowMS
+
+        // if start time is already passed for the day, set it to the next day
+        if (delay < 0) { delay += FULL_DAY_MS }
+        this.#setupTimers({ startMS, endMS, delay })
+    }
+
+    #setupTimers({ startMS, endMS, delay }) {
+        this._initialTimeout = setTimeout(async () => await this.#notifyAlert(), delay)
+
+        this._timer = setInterval(async () => {
+            const nowMS = getDateMS()
+            nowMS >= startMS && nowMS < endMS ? (await this.#notifyAlert()) : this.#clearTimers()
+        }, this._settings.interval * 60 * 1000)
+    }
+
+    async #notifyAlert() {
+        const { sound, alert_type } = this._settings
+        const progress = await this.#getProgress()
+
+        if (progress.percentage > 100) return
+
+        ;['notify', 'both'].includes(alert_type) && this.#notifyProgress(progress)
+        ;['alarm', 'both'].includes(alert_type) && this.#playSound(sound)
+    }
+
+    async #playSound(sound: string) {
+        await ensureOffscreenDocument()
         await sendOffscreenMessage(sound)
     }
 
-    create(type: NotificationType) {
-        const title = type == 'install' ? 'Welcome!' : 'Water Break Time!'
-        const message = type == 'install' 
-            ? 'Your journey to a well-hydrated future starts now!' 
-            : 'Glug glug glug! Time to hydrate.'
+    async #getProgress() {
+        const key = getDateKey()
+        const response = await getState(key) as DAILY_RESPONSE
+        const data = response[key]
+        const current = data.logs.reduce((acc, curr ) => acc + curr.amount, 0)
+        const percentage = Math.round((current / data.goal) * 100)
+        return  { goal: data.goal, left: data.goal - current, percentage }
+    }
 
+    #createNotification({ title, message }: { title: string, message: string }) {
         chrome.notifications.create({
             title,
             message,
             type: 'basic',
             iconUrl: chrome.runtime.getURL("src/icons/icon48.png")
+        })
+    }
+
+    #getEncouragingMessage(percentage: number): string {
+        // Calculate the start and end of the range based on percentage
+        const start = percentage - (percentage % 15)
+        const end = Math.min(start + 15, 100)
+
+        // Determine the category based on the start and end values
+        const category: string =  percentage >= 100 ? '100' : `${start}-${end}`;
+        return ENCOURAGEMENTS[category][Math.floor(Math.random() * ENCOURAGEMENTS[category].length)];
+    }
+
+    async #notifyProgress(progress: Progress) {
+        const { left, percentage, goal } = progress
+        const encouragement = this.#getEncouragingMessage(percentage)
+        const detail = percentage >= 100 ? `100%. ${goal} ml reached!` : `${percentage}% there. Only ${left}ml to reach goal.`
+
+        this.#createNotification({ 
+            title: 'Water Break Time! Time to Hydrate!',
+            message: `${encouragement} ${detail}`,
+        })
+    }
+
+    #createWelcome() {
+        this.#createNotification({ 
+            title: 'Welcome!',
+            message: 'Your journey to a well-hydrated life starts now!',
         })
     }
 }
