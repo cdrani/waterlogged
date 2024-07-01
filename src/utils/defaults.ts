0c062a8d06a1ce3d6941a2f5c9a3503c861c8cba
cdrani <charlesdrani@gmail.com>
2024-06-30 19:38:09 -0600

    docs: update defaults for settings & today values

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,20 +1,20 @@
 import type { SETTINGS, TODAY } from './types.d'
 
 export const SETTINGS_DEFAULT: SETTINGS = {
-    goal: 1800,
+    goal: 2000,
     amount: 250,
-    interval: 1, // minutes
+    interval: 60, // minutes
     enabled: true,
     sound: 'bubble1',
     measurement: 'ml',
-    end_time: '20:00',
+    end_time: '18:00',
     alert_type: 'both',
-    start_time: '00:00',
+    start_time: '08:00',
 }
 
 export const TODAY_DEFAULT: TODAY = {
     logs: [],
-    goal: 1800,
+    goal: 2000,
     amount: 250,
     measurement: 'ml'
 }
