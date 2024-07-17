553532880cf2c8801472b475b6bb38e744b1b97f
cdrani <charlesdrani@gmail.com>
2024-07-17 14:46:13 -0600

    docs: setup enhanced-image plugin

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { enhancedImages } from '@sveltejs/enhanced-img'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+        enhancedImages(),
+        sveltekit()
+    ]
 })
