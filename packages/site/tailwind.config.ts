e6709075814d21d61ccd40246126918862f35711
cdrani <charlesdrani@gmail.com>
2024-07-31 04:43:11 -0600

    chore: cleanup site tailwind config

--- packages/site/tailwind.config.ts
+++ packages/site/tailwind.config.ts
@@ -1,14 +1,3 @@
-// import type { Config } from 'tailwindcss'
-
-// export default {
-// 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-// 	theme: {
-// 		extend: {}
-// 	},
-
-// } as Config
-
 import type { Config } from 'tailwindcss'
 import sharedConfig from 'common/tailwind.config'
 
