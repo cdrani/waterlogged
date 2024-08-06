ab540b551b51c55c0aa1ed2c4029d14d81468ddf
cdrani <charlesdrani@gmail.com>
2024-08-06 15:13:16 -0600

    feat: add tooltip to Graph component

--- packages/common/views/Graph.svelte
+++ packages/common/views/Graph.svelte
@@ -17,8 +17,8 @@
     data.set("2024-05-11", 4)
 </script>
 
-<section class="flex relative flex-col mx-auto w-full h-[340px] max-h-[358] bg-cyan-200 p-4">
-    <div class="rounded-md overflow-x-scroll pb-4 bg-cyan-700 p-4">
+<section class="relative flex flex-col mx-auto w-full h-[340px] max-h-[358] bg-cyan-200 p-4">
+    <div class="relative flex flex-col w-full h-full p-4 rounded-md bg-cyan-800 overflow-x-scroll max-h-[160px]">
         <Graph 
             data={data}
             view={'yearly'}
@@ -29,10 +29,12 @@
             startDate={start}
             cellSize={12}
             cellRadius={6}
-            dayLabelWidth={32}
-            monthLabelHeight={24}
+            dayLabelWidth={36}
+            monthLabelHeight={20}
             dayLabels={['', 'Mon', '', 'Wed', '', 'Fri', '']}
             colors={['#404040', '#303030', '#202020', '#101010']}
         />
+
+        <div id="tooltip" class="absolute z-[1000] hidden min-w-[80px] rounded-[4px] top-4 border-gray-400 bg-white border-sm p-0.5 px-2 text-[10px]" />
     </div>
 </section>
