85dbe4bc8788fccae776ec6bdba2719fe10c7b5f
cdrani <charlesdrani@gmail.com>
2024-08-14 01:20:26 -0600

    feat: create Auth buttton & render in Nav

---
+++ packages/common/components/Auth.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { db } from 'common/data/db'
+    const userId = db.cloud.currentUserId
+
+    function openLogin() {
+        db.cloud.login()
+    }
+
+    async function logOut() {
+        await db.cloud.logout()
+
+    }
+</script>
+
+{#if userId == 'unauthorized'}
+    <button 
+        on:click={openLogin}
+        on:touchend={openLogin}
+        class="inline-flex justify-center self-center items-center w-20 h-8 bg-cyan-800 rounded-md"
+    >
+        <span class="font-bold text-sm text-white w-full">Log In</span>
+    </button>
+{:else}
+    <button 
+        on:click={logOut}
+        on:touchend={logOut}
+        class="inline-flex justify-center self-center items-center w-24 h-8 bg-cyan-800 rounded-md"
+    >
+        <span class="font-bold text-sm text-white w-full">Log Out</span>
+    </button>
+{/if}
