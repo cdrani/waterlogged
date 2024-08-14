cb7aadcd224ced91b2b57ca96a5b46ed831c7992
cdrani <charlesdrani@gmail.com>
2024-08-14 04:28:00 -0600

    refactor: extract save & cancel ui to FormActions

---
+++ packages/common/components/FormActions.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let onClose: () => void
+    export let submitLabel: string = 'Save'
+    export let cancelLabel: string = 'Cancel'
+</script>
+
+<div class="flex flex-end h-full w-full">
+    <div class="flex justify-end items-end w-full gap-4">
+        <button 
+            type="submit"
+            class="w-20 h-9 bg-white rounded-md"
+        >
+            <span class="font-semibold text-md text-black">{submitLabel}</span>
+        </button>
+
+        <button 
+            on:click={onClose}
+            class="w-20 h-9 bg-black rounded-md"
+        >
+            <span class="font-semibold text-md text-white">{cancelLabel}</span>
+        </button>
+    </div>
+</div>
