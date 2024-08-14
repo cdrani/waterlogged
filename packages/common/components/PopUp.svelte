d5e9b2895085f920e49c3575b4b31a261d12596f
cdrani <charlesdrani@gmail.com>
2024-08-14 03:25:17 -0600

    refactor: extract PopUp modal component

---
+++ packages/common/components/PopUp.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { clickOutside } from 'common/utils/click-outside'
+
+    export let onClose: () => void
+
+    const onKeyDown = (e: KeyboardEvent) => {
+        if (e.key !== 'Escape') return
+        onClose()
+    }
+</script>
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
+
+<div
+    aria-modal="true"
+    role="alertdialog"
+    class="fixed z-[100] w-full h-full overflow-y-hidden top-[184px] xs:top-1/2 -translate-y-1/2 shadow-black inset-0 flex mx-auto px-4 items-center content-center place-content-center"
+>
+    <div
+        class="absolute z-50 w-full"
+        use:clickOutside
+        on:click_outside={onClose}
+    >
+        <div class="border-white border-2 absolute z-[100] inset-0 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex flex-col w-[248px] xs:w-11/12 lg:w-1/3 mx-auto min-h-[225px] xs:min-h-[320px]  bg-cyan-500 rounded-md">
+            <button class="absolute z-[1001] top-1 right-1 w-5 h-5 cursor justify-end" on:click={onClose}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g fill="none" stroke="white" stroke-dasharray="22" stroke-dashoffset="22" stroke-linecap="round" stroke-width="3px">
+                        <path d="M19 5L5 19">
+                            <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.3s" dur="0.3s" values="22;0" />
+                        </path>
+                        <path d="M5 5L19 19">
+                            <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="22;0" />
+                        </path>
+                    </g>
+                </svg>
+            </button>
+        
+            <slot></slot>
+        </div>
+    </div>
+</div>
