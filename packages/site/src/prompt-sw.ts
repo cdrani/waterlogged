111573efc6b35aa49d2c5cf97b848c071ff4c2a5
cdrani <charlesdrani@gmail.com>
2024-08-21 22:50:31 -0600

    feat: create notification on background message

--- packages/site/src/prompt-sw.ts
+++ packages/site/src/prompt-sw.ts
@@ -3,6 +3,10 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 
+import { LogsService } from 'common/data/services'
+import { type MessagePayload } from 'firebase/messaging'
+import { getNotificationOptions, onBackgroundMessage } from './lib/firebase/messaging'
+
 import { NavigationRoute, registerRoute } from 'workbox-routing'
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 
@@ -11,10 +15,26 @@ declare let self: ServiceWorkerGlobalScope
 self.addEventListener('message', async (event) => {
     const type = event?.data?.type
     if (type === 'SKIP_WAITING') self.skipWaiting()
+
+    // if (type == 'SHOW_NOTIFICATION') {
+    //     const { title, notificationOptions } = event.data
+    //     self.registration.showNotification(title, notificationOptions)
+    // }
+})
+
+self.addEventListener('notificationclick', async event => {
+    await LogsService.addLogIntake()
+    event.notification.close()
 })
 
 self.addEventListener('activate', event => {
     event.waitUntil(self.clients.claim())
+})
+
+onBackgroundMessage(payload => {
+    const { data } = payload as MessagePayload
+    const notificationOptions = getNotificationOptions(data) as NotificationOptions
+    return self.registration.showNotification(data!.title, notificationOptions)
 })
 
 // self.__WB_MANIFEST is default injection point
