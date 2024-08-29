c624b697dc9575b518542ee0c87041e19be7f827
cdrani <charlesdrani@gmail.com>
2024-08-29 02:01:39 -0600

    feat: setup global shortcut for show/hide of app

--- packages/app/src-tauri/src/app/setup.rs
+++ packages/app/src-tauri/src/app/setup.rs
@@ -2,9 +2,13 @@ use std::error::Error;
 use tauri::App;
 
 use crate::app::tray;
+use crate::app::shortcut;
+
+pub const MAIN: &str = "main";
 
 pub fn init(app: &mut App) -> Result<() , Box<dyn Error>> {
     tray::init(app)?;
+    shortcut::init(app)?;
 
     Ok(())
 }
---
+++ packages/app/src-tauri/src/app/shortcut.rs
@@ -0,0 +1,27 @@
+use crate::app::setup::MAIN;
+use tauri::{App, Manager};
+use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
+
+pub fn init(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    app.handle().plugin(
+        tauri_plugin_global_shortcut::Builder::new()
+            .with_shortcuts(["ctrl+\\", "cmd+\\"])?
+            .with_handler(|app, shortcut, event| {
+                if event.state == ShortcutState::Pressed
+                    && (shortcut.matches(Modifiers::CONTROL, Code::Backslash) ||
+                        (shortcut.matches(Modifiers::SUPER, Code::Backslash)))
+                {
+                    let window = app.get_webview_window(MAIN).unwrap();
+
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                    }
+                }
+            })
+            .build(),
+    )?;
+
+    Ok(())
+}
