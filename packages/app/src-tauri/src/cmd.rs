89238d96cf7717b5ac066b6aae3e23be39060913
cdrani <charlesdrani@gmail.com>
2024-09-04 01:30:19 -0600

    feat: add cmds to update config file

--- packages/app/src-tauri/src/cmd.rs
+++ packages/app/src-tauri/src/cmd.rs
@@ -1,4 +1,24 @@
+use tauri::State;
+use crate::app::worker::{WorkerEvent, AsyncWorker};
+use crate::config::{settings::SettingsConfig, ConfigUpdateAction};
+
 #[tauri::command]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+pub async fn update_settings(worker: State<'_, AsyncWorker>, settings: SettingsConfig) -> Result<(), String> {
+    worker.send_event(WorkerEvent::UpdateSettings(settings)).await;
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn update_config(worker: State<'_, AsyncWorker>, action: ConfigUpdateAction) -> Result<(), String> {
+    worker.send_event(WorkerEvent::UpdateConfig(action.clone())).await;
+
+    // Check the key and restart the scheduler if needed
+    match action.key.as_str() {
+        "interval" | "start_time" | "end_time" | "enabled" => {
+            worker.send_event(WorkerEvent::StartNotifier).await;
+        },
+        _ => {}
+    }
+
+    Ok(())
 }
