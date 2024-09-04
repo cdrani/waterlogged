cb3053a40f0e777d03d6b61760729b70d7ef99de
cdrani <charlesdrani@gmail.com>
2024-09-04 01:58:56 -0600

    feat: connect events to trigger alarm + notification

--- packages/app/src/components/LogsView.svelte
+++ packages/app/src/components/LogsView.svelte
@@ -2,6 +2,9 @@
     import { onMount } from 'svelte'
     import { liveQuery } from 'dexie'
     import { writable } from 'svelte/store'
+    import { invoke } from '@tauri-apps/api/core'
+    import { listen } from '@tauri-apps/api/event'
+    import { isPermissionGranted, requestPermission } from '@tauri-apps/plugin-notification'
 
     import Nav from 'common/components/Nav.svelte'
     import GraphView from 'common/views/Graph.svelte'
@@ -11,10 +14,12 @@
     import SettingsView from 'common/views/Settings.svelte'
     import Celebrate from 'common/components/Celebrate.svelte'
 
-    import { type Messaging } from 'common/messaging'
-	import { initMessageHandler } from 'common/messaging'
+    import { playAlarm } from 'common/utils/alarm'
+    import type { ALERT, SETTINGS } from 'common/types'
+    import AppNotification from '../utils/notification'
     import { initModal, openModal } from 'common/stores/modal'
 	import { UserService, LogsService } from 'common/data/services'
+    import { type Messaging, initMessageHandler  } from 'common/messaging'
     import { type PartyStore, initParty, getParty } from 'common/stores/party'
 
     initModal()
@@ -31,14 +36,59 @@
     }
 
     let messaging: Messaging
+    let notifier: AppNotification
 
     // ensure a log exists for every day
     const loadOnMount = async () => {
         await LogsService.load()
         messaging = initMessageHandler()
+        notifier = new AppNotification()
     }
 
-    onMount(loadOnMount)
+    async function messageCB(action: { key: string, value: string | boolean | number }) {
+        await invoke('update_config', { action })
+    }
+
+    async function unMountCB(settings: Partial<SETTINGS>) {
+        await invoke('update_settings', { settings })
+    }
+
+    async function requestNotificationPermission() {
+        let permissionGranted = await isPermissionGranted();
+
+        // If not we need to request it
+        if (!permissionGranted) {
+          const permission = await requestPermission()
+          permissionGranted = permission === 'granted'
+        }
+
+        return permissionGranted
+    }
+
+    async function sendNotification() {
+        const permissionGranted = await requestNotificationPermission()
+
+        // TODO: instead show notification/modal to grant permission?
+        if (!permissionGranted) return
+
+        await notifier.notify()
+    }
+
+    type Payload = { sound: string, alert_type: ALERT }
+
+    onMount(() => {
+        (async () => {
+            loadOnMount()
+
+            const unlisten = await listen<Payload>('send_alert', async (event) => {
+                const { alert_type, sound } = event.payload
+                if (['alarm', 'both'].includes(alert_type)) await playAlarm(sound)
+                if (['notify', 'both'].includes(alert_type)) await sendNotification()
+            })
+
+            return () => unlisten()
+        })()
+    })
 
 	const user = liveQuery(async () => await UserService.getUser())
     let log = liveQuery(async () => await LogsService.getByDate())
@@ -61,7 +111,7 @@
             {#if $pageView == 'default'}
                 <DefaultView />
             {:else if $pageView == 'settings'}
-                <SettingsView messaging={messaging} />
+                <SettingsView {messaging} {messageCB}  {unMountCB} />
             {:else}
                 <GraphView log={$log} />
             {/if}
@@ -81,4 +131,3 @@
         <span></span>
     </div>
 {/if}
-
