151550f2f490b5b4f1405769af3623b42aa88732
cdrani <charlesdrani@gmail.com>
2024-09-04 02:00:49 -0600

    refactor: switch cancel and submit buttons order

--- packages/common/components/FormActions.svelte
+++ packages/common/components/FormActions.svelte
@@ -7,17 +7,18 @@
 <div class="flex flex-end h-full w-full">
     <div class="flex justify-end items-end w-full gap-4">
         <button 
+            on:click={onClose}
+            class="w-16 h-7 md:w-20 md:h-9 bg-black rounded-md"
+        >
+            <span class="font-semibold text-[14px] md:text-[16px] text-white">{cancelLabel}</span>
+        </button>
+
+        <button 
             type="submit"
             class="w-16 h-7 md:w-20 md:h-9 bg-white rounded-md"
         >
             <span class="font-semibold text-[14px] md:text-[16px] text-black">{submitLabel}</span>
         </button>
 
-        <button 
-            on:click={onClose}
-            class="w-16 h-7 md:w-20 md:h-9 bg-black rounded-md"
-        >
-            <span class="font-semibold text-[14px] md:text-[16px] text-white">{cancelLabel}</span>
-        </button>
     </div>
 </div>
