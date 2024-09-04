6ba28cec43fe02d1e1ba8f1186c368362f993b00
cdrani <charlesdrani@gmail.com>
2024-09-04 01:25:34 -0600

    feat: on init listen to worker events + start notifier

--- packages/app/src-tauri/src/app/setup.rs
+++ packages/app/src-tauri/src/app/setup.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use tauri::App;
+use tauri::{Manager, App};
 
 use crate::app::tray;
 use crate::app::launcher;
 use crate::app::shortcut;
+use crate::app::worker::Worker;
 
 pub const MAIN: &str = "main";
 
@@ -11,6 +12,16 @@ pub fn init(app: &mut App) -> Result<() , Box<dyn Error>> {
     tray::init(app)?;
     shortcut::init(app)?;
     launcher::init(app)?;
+
+    let (mut worker, async_worker) = Worker::new(app.handle().clone());
+
+    app.manage(async_worker);
+
+    worker.listen();
+
+    tauri::async_runtime::spawn(async move {
+        worker.start_notifier().await;
+    });
 
     Ok(())
 }
