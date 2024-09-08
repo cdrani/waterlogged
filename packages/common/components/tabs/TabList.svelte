4c275d3faca5dccb913f264d8a6bd45fa69993f8
cdrani <charlesdrani@gmail.com>
2024-09-08 01:15:28 -0600

    feat: stub out components for Account tabs view

---
+++ packages/common/components/tabs/TabList.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { writable } from 'svelte/store'
+    type Tab = { title: string, content: any }
+    export let tabs: Tab[] = []
+
+    let activeTab = writable<string>(tabs[0].title)
+
+    const setActiveTab = (tabTitle: string) => activeTab.set(tabTitle)
+</script>
+
+<style lang="postcss">
+    .active {
+        @apply bg-cyan-500 text-white;
+    }
+</style>
+
+<div class="flex-col w-full mx-auto items-center">
+    <div role="tablist" class="relative flex w-full place-items-center">
+        {#each tabs as tab (tab.title)}
+            <a role="tab" 
+                class="px-4 py-2 cursor-pointer bg-cyan-800 text-white hover:text-white hover:font-bold"
+                class:active={$activeTab == tab.title}
+                on:click|preventDefault={() => setActiveTab(tab.title)}
+                on:touchend|preventDefault={() => setActiveTab(tab.title)}
+            >
+                {tab.title}
+            </a>
+        {/each}
+    </div>
+
+    {#each tabs as tab (tab.title)}
+        {#if $activeTab === tab.title}
+            <div class="flex">
+                <svelte:component this={tab.content} />
+            </div>
+        {/if}
+    {/each}
+</div>
