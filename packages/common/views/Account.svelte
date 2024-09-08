d88205f8db50989b99ae02fa4cb45a0b7a56ef23
cdrani <charlesdrani@gmail.com>
2024-09-08 01:15:57 -0600

    feat: create Account view to render active tab

---
+++ packages/common/views/Account.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { db } from 'common/data/db'
+    import TabList from 'common/components/tabs/TabList.svelte'
+    import GeneralTab from 'common/components/tabs/GeneralTab.svelte'
+    import AccountTab from 'common/components/tabs/AccountTab.svelte'
+    import AdvancedTab from 'common/components/tabs/AdvancedTab.svelte'
+
+    let tabs = [
+        { title: 'General', content: GeneralTab },
+        { title: 'Advanced', content: AdvancedTab }
+     ]
+
+    let userId = db.cloud.currentUserId
+
+    $: if (userId) {
+        if (userId == 'unauthorized') {
+            tabs.splice(1, 0, { title: 'Account', content: AccountTab })
+        }
+    }
+</script>
+
+<section class="flex relative left-0 flex-col mx-auto w-full h-[340px] max-h-[358] xs:pb-6 xs:h-full bg-cyan-200">
+    <div class="relative flex flex-col w-full mx-auto overflow-y-auto h-full">
+        <div class="flex mx-auto">
+            <TabList {tabs} />
+        </div>
+    </div>
+</section>
