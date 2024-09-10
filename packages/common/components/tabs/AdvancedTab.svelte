6782d63d669ae23ced19dcc3d6b9f91a865a9ac0
cdrani <charlesdrani@gmail.com>
2024-09-10 00:45:15 -0600

    refactor: separate app and common Account views

--- packages/common/components/tabs/AdvancedTab.svelte
+++ packages/common/components/tabs/AdvancedTab.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+    import { importDB } from 'dexie-export-import'
+
+    import { db } from 'common/data/db'
+    import DBData from '../DBData.svelte'
+    import { downloadBackUp } from 'common/utils/data'
+
+    async function exportData() {
+        await downloadBackUp()
+    }
+
+    async function importData(file: File) {
+        await db.delete()
+        await importDB(file)
+    }
 </script>
 
-<h1>advanced tab</h1>
+<DBData {exportData} {importData} />
