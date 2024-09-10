b2bf7596d67b0ec9f4b4951ac649e947df12831f
cdrani <charlesdrani@gmail.com>
2024-09-09 23:53:43 -0600

    styles: round tab list tabs

--- packages/common/components/tabs/TabList.svelte
+++ packages/common/components/tabs/TabList.svelte
@@ -7,6 +7,13 @@
     let activeTab = writable<string>(tabs[0].title)
 
     const setActiveTab = (tabTitle: string) => activeTab.set(tabTitle)
+
+    const getStyles = (title: string) => {
+        const rounding = tabs.at(0).title == title 
+            ? 'rounded-tl-md' : tabs.at(-1).title == title
+            ? 'rounded-tr-md' : ''
+        return `${rounding}  w-full text-center px-4 py-2 cursor-pointer bg-cyan-800 text-white hover:text-white`
+    }
 </script>
 
 <style lang="postcss">
@@ -19,7 +26,7 @@
     <div role="tablist" class="relative flex w-full place-items-center">
         {#each tabs as tab (tab.title)}
             <a role="tab" 
-                class="px-4 py-2 cursor-pointer bg-cyan-800 text-white hover:text-white"
+                class="{getStyles(tab.title)}"
                 class:active={$activeTab == tab.title}
                 on:click|preventDefault={() => setActiveTab(tab.title)}
                 on:touchend|preventDefault={() => setActiveTab(tab.title)}
@@ -31,9 +38,7 @@
 
     {#each tabs as tab (tab.title)}
         {#if $activeTab === tab.title}
-            <div class="flex">
-                <svelte:component this={tab.content} />
-            </div>
+            <svelte:component this={tab.content} />
         {/if}
     {/each}
 </div>
