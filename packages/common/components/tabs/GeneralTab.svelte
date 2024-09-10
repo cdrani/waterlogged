6f30fbf47da62d6d8e009c06f395aed25efe6364
cdrani <charlesdrani@gmail.com>
2024-09-10 02:01:48 -0600

    styles: update GeneralTab ui + href values

--- packages/common/components/tabs/GeneralTab.svelte
+++ packages/common/components/tabs/GeneralTab.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { VERSION } from 'common/utils/version'
 </script>
 
 <!-- <div class="flex justify-between items-center"> -->
@@ -15,19 +16,37 @@
     <!-- /> -->
 <!-- </div> -->
 
-<div class="w-full flex space-x-4">
-    <a
-        target="_blank"
-        class="underline underline-offset-4 text-sm"
-        href="https://github.com/cdrani/waterlogged/issues"
-    >
-        Report an issue
-    </a>
-    <a
-        target="_blank"
-        href="mailto:charlesdrani@gmai.com"
-        class="underline underline-offset-4 text-sm"
-    >
-        Send an email
-    </a>
+<div class="flex flex-col w-full mx-auto">
+    <div class="flex w-full justify-between items-center bg-cyan-400 p-4 rounded-md my-4">
+        <p class="font-bold text-md">{VERSION}</p>
+        <a
+            target="_blank"
+            href="https://github.com/cdrani/waterlogged/issues"
+            class="inline-flex text-sm justify-center self-center items-center w-28 h-10 bg-white rounded-md"
+        >
+            <span class="underline underline-offset-4 tracking-wide font-bold text-center text-black w-full">View Code</span>
+        </a>
+    </div>
+
+    <div class="flex-col w-full justify-between items-center bg-cyan-400 p-4 rounded-md">
+        <p class="font-bold text-md text-center">Questions | Suggestions</p>
+
+        <div class="flex w-full justify-between items-center bg-cyan-400 rounded-md my-4">
+            <a
+                target="_blank"
+                href="https://forms.gle/SR72HpB9PawZLpjv7"
+                class="inline-flex text-sm justify-center self-center items-center w-28 h-10 bg-white rounded-md"
+            >
+                <span class="underline underline-offset-4 tracking-wide font-bold text-center text-black w-full">Feedback</span>
+            </a>
+
+            <a
+                target="_blank"
+                href="mailto:charlesdrani@gmai.com"
+                class="inline-flex text-sm justify-center self-center items-center w-28 h-10 bg-cyan-800 rounded-md"
+            >
+                <span class="underline underline-offset-4 tracking-wide font-bold text-center text-white w-full">Send Email</span>
+            </a>
+        </div>
+    </div>
 </div>
