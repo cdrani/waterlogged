324464570aa025d20ca2e30719865f854396ade0
cdrani <charlesdrani@gmail.com>
2024-09-15 23:26:37 -0600

    fix: set vite server config for extension

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -7,6 +7,13 @@ import manifest from './manifest.json'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte(), crx({ manifest })],
+    server: {
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            port: 5173
+        }
+    },
     build: {
         rollupOptions: {
             input: {
