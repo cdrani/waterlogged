75da2e2fd1fb8c032a8f2fe389f725ad0d32913a
cdrani <charlesdrani@gmail.com>
2024-09-18 19:22:13 -0600

    feat: setup app manual update check and install ui

--- packages/common/components/tabs/GeneralTab.svelte
+++ packages/common/components/tabs/GeneralTab.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-    import { VERSION } from 'common/utils/version'
+    import { onMount } from 'svelte'
+    import { getAppVersion } from 'common/utils/version'
+
+    let version: string = "v1.0.0"
+    export let isApp: boolean = false
+    export let manualUpdate: () => void
+
+    async function getVersion() {
+        version = await getAppVersion()
+    }
+
+    onMount(getVersion)
 </script>
 
 <!-- <div class="flex justify-between items-center"> -->
@@ -18,20 +29,42 @@
 
 <div class="flex flex-col w-full h-full mx-auto">
     <div class="flex w-full justify-between items-center bg-cyan-400 p-4 rounded-md mt-4 mb-5">
-        <p class="font-bold text-lg">{VERSION}</p>
+        <div class="flex flex-col">
+            <p class="font-bold text-lg" class:pb-2={isApp}>{version}</p>
+
+            {#if isApp}
+                <div class="flex self-end">
+                <button 
+                    on:click|preventDefault={manualUpdate}
+                    class="inline-flex text-sm justify-center self-center items-center w-24 xs:w-28 h-10 bg-white rounded-md"
+                >
+                    <span class="underline underline-offset-4 tracking-normal font-bold text-center text-black w-full">
+                        Update App 
+                    </span>
+                </button>
+                </div>
+            {/if}
+        </div>
+
         <a
             target="_blank"
             href="https://github.com/cdrani/waterlogged"
-            class="inline-flex text-sm justify-center self-center items-center w-28 h-10 bg-white rounded-md"
+            class="inline-flex text-sm justify-end self-end items-center w-24 xs:w-28 h-10 {isApp ? 'bg-cyan-800' : 'bg-white'} rounded-md"
         >
-            <span class="underline underline-offset-4 tracking-wide font-bold text-center text-black w-full">View Code</span>
+            <span 
+                class="underline underline-offset-4 tracking-wide font-bold text-center text-black w-full"
+                class:text-white={isApp}
+                class:track-normal={isApp}
+            >
+                View Code
+            </span>
         </a>
     </div>
 
     <div class="flex-col w-full justify-between items-center bg-cyan-400 p-4 rounded-md">
         <p class="font-bold text-lg text-center">Questions | Suggestions</p>
 
-        <div class="flex w-full justify-between items-center bg-cyan-400 rounded-md my-4">
+        <div class="flex w-full justify-between items-center bg-cyan-400 rounded-md mt-4">
             <a
                 target="_blank"
                 href="https://forms.gle/SR72HpB9PawZLpjv7"
@@ -42,7 +75,7 @@
 
             <a
                 target="_blank"
-                href="mailto:charlesdrani@gmai.com"
+                href="mailto:charlesdrani@gmail.com"
                 class="inline-flex text-sm justify-center self-center items-center w-24 xs:w-28 h-10 bg-cyan-800 rounded-md"
             >
                 <span class="underline underline-offset-4 tracking-wide font-bold text-center text-white w-full">Send Email</span>
