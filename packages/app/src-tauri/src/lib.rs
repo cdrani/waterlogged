b557c7ab362bf198e6998aa9710622993206993c
cdrani <charlesdrani@gmail.com>
2024-09-18 19:07:22 -0600

    config: setup updater + dialog plugins

--- packages/app/src-tauri/src/lib.rs
+++ packages/app/src-tauri/src/lib.rs
@@ -7,11 +7,20 @@ mod config;
 
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec![])))
-        .setup(app::setup::init)
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
+            app::setup::init(app)
+        })
         .on_window_event(app::window::init)
         .invoke_handler(tauri::generate_handler![
             cmd::update_config,
