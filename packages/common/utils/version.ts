39ebe93801f943566548afb42b6c405d0e6ed6ad
cdrani <charlesdrani@gmail.com>
2024-09-18 19:08:03 -0600

    refactor: update version to be set from app

--- packages/common/utils/version.ts
+++ packages/common/utils/version.ts
@@ -1,1 +1,11 @@
-export const VERSION = 'v0.1.0'
+export async function getAppVersion() {
+    const VERSION = 'v0.1.0'
+    const app = await import('@tauri-apps/api/app')
+
+    if (app) {
+        const version = await app.getVersion()
+        return `v${version}`
+    }
+
+    return VERSION
+}
