aae355881981812f52135c6171de24f17ff3176c
cdrani <charlesdrani@gmail.com>
2024-09-23 18:47:19 -0600

    fix: ensure celebration ends on complete view close

--- packages/common/components/Modal.svelte
+++ packages/common/components/Modal.svelte
@@ -3,15 +3,14 @@
     import Congratulation from './Congratulation.svelte'
     import AddLogForm from './forms/log/AddLogForm.svelte'
 
+    import { endParty } from 'common/stores/party'
     import { closeModal, getModal } from 'common/stores/modal'
-    import { getParty, type PartyStore } from 'common/stores/party'
 
     const modal = getModal()
-    const partyStore = getParty() as PartyStore
 
     function handleClose() {
         const view = $modal.view
-        if (view == 'complete') (partyStore.partied = true)
+        if (view == 'complete') endParty()
         closeModal()
     }
 </script>
