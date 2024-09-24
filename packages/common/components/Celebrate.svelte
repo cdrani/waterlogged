80c841f3df825d6690054ed5279d436f1f8b9925
cdrani <charlesdrani@gmail.com>
2024-09-23 19:45:41 -0600

    fix: end celebration on Congratulation close

--- packages/common/components/Celebrate.svelte
+++ packages/common/components/Celebrate.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
     import { Confetti } from 'svelte-confetti'
     import { getParty, endParty } from 'common/stores/party'
+    import { closeModal } from 'common/stores/modal'
 
     const party = getParty()
 
     let timeoutId: ReturnType<typeof setTimeout>
 
     $: if ($party.celebrate) {
-        // End celebration after 7 seconds
+        // End celebration after 10 seconds
         clearTimeout(timeoutId)
-        timeoutId = setTimeout(endParty, 7000)
+        timeoutId = setTimeout(() => {
+            endParty(); closeModal()
+        }, 10_000)
     }
 </script>
 
 {#if $party.celebrate}
-    <div class="fixed pointer-events-none h-screen w-screen flex justify-center left-0 top-[-50px] z-[1000000]">
-        <Confetti x={[-5, 5]} y={[0, 0.1]} infinite duration={5000} amount={2000} fallDistance="300vh" />
+    <div class="fixed pointer-events-none h-screen w-full flex justify-center  z-[1000000]">
+        <Confetti x={[-5, 5]} y={[-5, 0.1]} infinite duration={5000} amount={2000} fallDistance="300vh" />
     </div>
 {/if}
