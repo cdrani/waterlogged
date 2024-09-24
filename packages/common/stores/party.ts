a5825effd673053801e6f8ac938be3041f18ba2a
cdrani <charlesdrani@gmail.com>
2024-09-23 18:28:38 -0600

    refactor: rewrite party store as functions

--- packages/common/stores/party.ts
+++ packages/common/stores/party.ts
@@ -2,52 +2,27 @@ import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 import { getContext, setContext } from 'svelte'
 
-export class PartyStore {
-    private party: Writable<boolean>
-
-    constructor() {
-        this.party = writable<boolean>(this.partied)
-    }
-
-    get partied() {
-        if (typeof window !== 'undefined' && window.localStorage) {
-            const hasPartied = JSON.parse(localStorage.getItem('confetti_shown') || 'false')
-            if (hasPartied == null) {
-                localStorage.setItem('confetti_shown', 'false')
-                return false
-            }
-            return hasPartied
-        }
-
-        return false // Default value if localStorage is not available
-    }
-
-    set partied(partied: boolean) {
-        if (typeof window !== 'undefined' && window.localStorage) {
-            localStorage.setItem('confetti_shown', JSON.stringify(partied))
-        }
-
-        this.party.set(partied)
-    }
-
-    canParty(complete: boolean) {
-        if (!complete) {
-            this.partied = false
-            return false
-        }
-
-        if (this.partied) return false
-
-        return true
-    }
-}
-
 const STORE = 'party'
 
-export function getParty() {
-    return getContext(STORE)
+type Party = { celebrate: boolean }
+type Context = Writable<Party>
+
+export const INIT_PARTY: Party = { celebrate: false }
+
+const party = writable(INIT_PARTY)
+ 
+export function initParty() {
+    setContext(STORE, party)
 }
 
-export function initParty() {
-    setContext(STORE, new PartyStore())
+export function getParty() {
+    return getContext<Context>(STORE)
+}
+
+export function startParty() {
+    party.set({ celebrate: true })
+}
+
+export function endParty() {
+    party.set({ celebrate: false })
 }
