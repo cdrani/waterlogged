800688ad891419956347e90da6c296154fdf3c9e
cdrani <charlesdrani@gmail.com>
2024-09-23 18:50:34 -0600

    feat: end celebration after 7 seconds

--- packages/common/components/Celebrate.svelte
+++ packages/common/components/Celebrate.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
     import { Confetti } from 'svelte-confetti'
+    import { getParty, endParty } from 'common/stores/party'
 
-    export let party: boolean = false 
+    const party = getParty()
+
+    let timeoutId: ReturnType<typeof setTimeout>
+
+    $: if ($party.celebrate) {
+        // End celebration after 7 seconds
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(endParty, 7000)
+    }
 </script>
 
-{#if party}
-    <div class="fixed pointer-events-none h-screen w-screen flex justify-center left-0 top-[-50px] z-[100]">
+{#if $party.celebrate}
+    <div class="fixed pointer-events-none h-screen w-screen flex justify-center left-0 top-[-50px] z-[1000000]">
         <Confetti x={[-5, 5]} y={[0, 0.1]} infinite duration={5000} amount={2000} fallDistance="300vh" />
     </div>
 {/if}
