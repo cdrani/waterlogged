0e81af12ee7ecc4640bf04f8011537ba5ae9fcd8
cdrani <charlesdrani@gmail.com>
2024-09-23 19:46:39 -0600

    style: fix Congratulation ui + font sizing

--- packages/common/components/Congratulation.svelte
+++ packages/common/components/Congratulation.svelte
@@ -13,24 +13,27 @@
 </script>
 
 {#if $log}
-    <section class="absolute z-[1000] flex flex-col w-full h-full p-4 pt-6">
-        <h1 class="text-center font-bold text-lg md:text-xl lg:text-3xl">Congratulations!</h1>
-        <h2 class="text-center text-2xl font-medium my-4">You hit your daily goal!</h2>
-        <h3 class="text-center text-white text-2xl font-black">
+    <section class="absolute z-[1000] flex flex-col w-full h-full gap-y-2 xs:gap-0 md:gap-y-2 p-4 xs:pt-6 lg:p-8">
+        <h1 class="text-center text-white font-bold text-xl xs:text-2xl lg:text-3xl">Congratulations!</h1>
+        <h2 class="text-center text-3xl md:text-4xl lg:text-5xl font-bold my-1 xs:my-2 md:my-3">Goal Reached!</h2>
+        <h3 class="text-center text-white text-xl md:text-2xl font-black">
             {$log.total} / {$log.goal} ({$log.measurement})
         </h3>
 
-        <p class="text-center font-semibold text-md md:text-lg lg:text-xl mt-2 leading-snug">
+        <p class="text-center font-semibold text-lg xs:text-xl lg:text-2xl mt-2 leading-snug">
             Keep your streak going tomorrow!
-            Adjust your targets as necessary.
         </p>
 
-        <button 
-            on:click|preventDefault={handleClose}
-            on:touchend|preventDefault={handleClose}
-            class="absolute bottom-8 right-8 w-20 h-9 bg-white rounded-md"
-        >
-            <span class="font-bold text-[14px] md:text-[16px]">Close</span>
-        </button>
+        <div class="flex flex-end h-full w-full">
+            <div class="flex justify-end items-end w-full gap-4">
+                <button 
+                    class="w-20 h-9 bg-white rounded-md"
+                    on:click|preventDefault={handleClose}
+                    on:touchend|preventDefault={handleClose}
+                >
+                    <span class="font-semibold text-[14px] md:text-base">Close</span>
+                </button>
+            </div>
+        </div>
     </section>
 {/if}
