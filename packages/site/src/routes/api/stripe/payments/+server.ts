ef452e38c86250e1f3ae9e33d3b7ebeec3e2ec52
cdrani <charlesdrani@gmail.com>
2024-09-28 15:45:05 -0600

    refactor: rewrite /api/stripe/payments POST handler

--- packages/site/src/routes/api/stripe/payments/+server.ts
+++ packages/site/src/routes/api/stripe/payments/+server.ts
@@ -1,34 +1,40 @@
 import Stripe from 'stripe'
+import { json } from '@sveltejs/kit'
+import type { RequestHandler } from '@sveltejs/kit'
 
-const stripe = new Stripe(import.meta.env.VITE_STRIPE_API_KEY!, {
-	typescript: true,
-})
+const stripe = new Stripe(import.meta.env.VITE_STRIPE_API_KEY, { typescript: true })
 
-export async function POST(request: Request) {
-	let data = await request.json()
-	let userId = data.userId
+export const POST: RequestHandler = async ({ request }) => {
+    try {
+        const { userId, ref } = await request.json()
 
-	// fetch customer, create if one doesn't exist
-	let customer
+        // fetch customer, create if one doesn't exist
+        let customer
 
-	const allCustomers = (await stripe.customers.list({ email: userId })).data
+        const allCustomers = (await stripe.customers.list({ email: userId })).data
 
-	if (allCustomers.length > 0) customer = allCustomers[0].id
-	else {
-		const newCustomer = await stripe.customers.create({ email: userId })
-		customer = newCustomer.id
-	}
-
-	const session = await stripe.checkout.sessions.create({
-		customer,
-		mode: 'subscription',
-		line_items: [{
-            quantity: 1,
-            price: import.meta.env.STRIPE_PRICE_ID,
-		}],
-		cancel_url: import.meta.env.VITE_STRIPE_CANCEL_URL,
-		success_url: import.meta.env.VITE_STRIPE_SUCCESS_URL,
-	})
-
-	return Response.json(session.url)
+        if (allCustomers.length > 0) {
+            customer = allCustomers[0].id
+        } else {
+            const newCustomer = await stripe.customers.create({ email: userId })
+            customer = newCustomer.id
+        }
+        
+        const session = await stripe.checkout.sessions.create({
+            customer,
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            line_items: [{
+                quantity: 1,
+                price: import.meta.env.VITE_STRIPE_PRICE_ID,
+            }],
+            cancel_url: `${import.meta.env.VITE_STRIPE_CANCEL_URL}?ref=${ref}`,
+            success_url: `${import.meta.env.VITE_STRIPE_SUCCESS_URL}?ref=${ref}`,
+        })
+        
+	    return json(session.url)
+    } catch (error) {
+        console.error('Error in Stripe connection:', error)
+        return json({ error: 'Failed to connect to Stripe' }, { status: 500 })
+    }
 }
