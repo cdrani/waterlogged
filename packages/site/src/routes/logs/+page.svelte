e58ab7c87e39860d761ff1d8d07a04754c99c6dc
cdrani <charlesdrani@gmail.com>
2024-09-28 16:16:12 -0600

    feat(site): re-sync dexie on payments auth flow

--- packages/site/src/routes/logs/+page.svelte
+++ packages/site/src/routes/logs/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte'
+    import { page } from '$app/stores'
     import { writable } from 'svelte/store'
 
     import { db } from 'common/data/db'
@@ -21,9 +22,19 @@
         if ($syncState?.status === 'offline') loading.set(false)
 	}
 
-	$: $syncState?.status, sync()
+    const resync = async () => {
+        if (params.get('payments')) {
+            // @ts-ignore
+            await db.$logins.toCollection().modify({ accessTokenExpiration: new Date() })
+        }
+    }
 
-	onMount(sync)
+	$: $syncState?.status, sync()
+    $: params = $page.url.searchParams
+
+	onMount(() => {
+        sync(); resync()
+    })
 </script>
 
 <svelte:head>
