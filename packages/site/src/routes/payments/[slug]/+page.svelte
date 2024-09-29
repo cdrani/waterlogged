dd4c7770eacf4a4efc396a5266402b7ed15c191a
cdrani <charlesdrani@gmail.com>
2024-09-28 22:20:56 -0600

    chore: cleanup warnings in error + payments pages

--- packages/site/src/routes/payments/[slug]/+page.svelte
+++ packages/site/src/routes/payments/[slug]/+page.svelte
@@ -8,7 +8,7 @@
     export let data: PageData
     let countdown = 10
     let message = ''
-    let interval: Timer | null = null
+    let interval: Timer
 
     $: params = $page.url.searchParams
 
@@ -22,16 +22,11 @@
                 startCountdown(() => goto(`/logs?payments=${data!.status}`))
             })
         }
-
-        // @ts-ignore
-        await db.$logins.toCollection().modify({ accessTokenExpiration: new Date() })
     }
 
     const updateMessage = () => {
         const ref = params.get('ref')
-        if (ref === 'web') {
-            message = `Redirecting back to Logs page in ${countdown} seconds.`;
-        }
+        if (ref === 'web') message = `Redirecting back to Logs page in ${countdown} seconds.`
     }
 
     const startCountdown = (callback: () => void) => {
@@ -53,7 +48,7 @@
 
 <main class="flex flex-col relative gap-10 w-full h-screen max-w-screen-md mx-auto p-5 md:p-8 xl:p-16">
     {#if params.get('ref')}
-        <h2 class="p-4 xs:p-6 md:p-10 max-w-screen-md bg-cyan-600 rounded-md text-white text-lg md:text-2xl lg:text-3xl">{message.replace('5', countdown)}</h2>
+        <h2 class="p-4 xs:p-6 md:p-10 max-w-screen-md bg-cyan-600 rounded-md text-white text-lg md:text-2xl lg:text-3xl">{message.replace('5', `${countdown}`)}</h2>
     {/if}
     <div class="flex flex-col justify-center items-center gap-4 md:gap-8 lg:gap-10 bg-cyan-800 border-md border-white rounded-md p-6 md:p-10">
         <h1 class="text-white text-center font-bold text-2xl xs:text-3xl md:text-4xl lg:text-6xl">{data.title}</h1>
