2c8565ba3c1c73b370cf9d0a30ddc7af6b66b83b
cdrani <charlesdrani@gmail.com>
2024-09-28 22:11:21 -0600

    refactor: add ref to api/stripe/payments body

--- packages/common/components/License.svelte
+++ packages/common/components/License.svelte
@@ -3,31 +3,40 @@
     import EvalDays from './EvalDays.svelte'
 
     const user = db.cloud.currentUser
-
-    async function handleUpgrade() {
-        const response = await fetch('/api/stripe/payment', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ userId: $user.userId })
-        })
-
-        const url = await response.json()
-        window.location.assign(url)
-    }
-
     let licenseType: string = 'Free'
 
     $: if ($user) {
         licenseType = !$user?.license ? 'Free' : $user?.license?.type == 'eval' ? 'Free' : 'Pro'
     }
+
+    const isCRX = () => typeof chrome !== 'undefined' && chrome.runtime
+
+    async function handleUpgrade() {
+        try {
+            const response = await fetch(import.meta.env.VITE_API_PATH, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ userId: $user.userId, ref: isCRX() ? 'crx' : 'web' })
+            })
+
+            const url = await response.json()
+            if (!url) return
+
+            if (typeof chrome !== 'undefined' && chrome.runtime) {
+                chrome.tabs.create({ url })
+            } else {
+                window.location.assign(url)
+            }
+        } catch (error) {
+            console.error('Error in handleUpgrade: ', error)
+        }
+    }
 </script>
 
-{#if !$user?.license}
+{#if $user?.license}
     <div class="flex w-full justify-between items-center p-4 bg-cyan-400 rounded-md">
         <div class="flex-col">
-            <p class="font-bold text-md">Tier: {licenseType}</p>
+            <p class="font-bold text-base min-w-20">Tier: {licenseType}</p>
             {#if licenseType == 'Free'}
                 <EvalDays evalDays={$user?.license?.evalDaysLeft} />
             {/if} 
@@ -35,23 +44,25 @@
 
         {#if licenseType == 'Free'}
             <button 
+                type="button"
                 on:click|preventDefault={handleUpgrade}
                 on:touchend|preventDefault={handleUpgrade}
-                class="inline-flex justify-center self-center items-center w-20 h-10 bg-cyan-800 rounded-md"
+                class="inline-flex cursor-pointer justify-center self-center items-center w-20 xs:w-24 h-10 bg-white rounded-md"
             >
-                <span class="font-bold text-sm text-white w-full">Upgrade</span>
+                <span class="font-bold text-base w-full">Upgrade</span>
             </button>
         {/if} 
 
         {#if licenseType == 'Pro'}
             <a 
                 target="_blank"
-                on:click|preventDefault={handleUpgrade}
-                on:touchend|preventDefault={handleUpgrade}
-                class="inline-flex justify-center self-center items-center w-44 h-10 bg-white rounded-md"
+                href={import.meta.env.VITE_STRIPE_CUSTOMER_PORTAL}
+                class="inline-flex cursor-pointer justify-center self-center items-center w-36 h-10 bg-white rounded-md"
             >
-                <span class="font-bold text-center text-sm text-black w-full">Manage Subscription</span>
+                <span class="font-bold text-center text-base text-black w-full">Manage Tier</span>
             </a>
         {/if}
     </div>
+
+    <hr class="w-full h-1 bg-cyan-600 my-4" />
 {/if}
