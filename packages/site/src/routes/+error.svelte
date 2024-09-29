177b3f7b4f3e1bef43035aad43f45d430d66e2cf
cdrani <charlesdrani@gmail.com>
2024-09-28 22:20:56 -0600

    chore: cleanup warnings in error + payments pages

--- packages/site/src/routes/+error.svelte
+++ packages/site/src/routes/+error.svelte
@@ -4,7 +4,7 @@
 	import { onMount, onDestroy } from 'svelte'
 
     let countdown = 5
-    let interval: Timer | null = null
+    let interval: Timer
     let message = 'Redirecting back to the Logs page in 5 seconds.'
 
     const resync = async () => {
@@ -35,7 +35,7 @@
 </script>
 
 <main class="flex flex-col relative gap-10 w-full h-screen max-w-screen-md mx-auto p-5 md:p-8 xl:p-16">
-    <h2 class="p-4 xs:p-6 md:p-10 max-w-screen-md bg-cyan-600 rounded-md text-white text-lg md:text-2xl lg:text-3xl">{message.replace('5', countdown)}</h2>
+    <h2 class="p-4 xs:p-6 md:p-10 max-w-screen-md bg-cyan-600 rounded-md text-white text-lg md:text-2xl lg:text-3xl">{message.replace('5', `${countdown}`)}</h2>
     <div class="flex flex-col justify-center items-center gap-4 md:gap-8 lg:gap-10 bg-cyan-800 border-md border-white rounded-md p-6 md:p-10">
         <h1 class="text-white text-center font-bold text-2xl xs:text-3xl md:text-4xl lg:text-6xl">Error</h1>
         {#if $page?.error?.message}
